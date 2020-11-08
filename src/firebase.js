970f1525da7ba9bca0a3c138574a6a457b635520
kingtrungtruc <1660659@student.hcmus.edu.vn>
2020-11-08 22:41:09 +0700

    init project

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCsi2eZxQvcGz95OESGeY_53fQInp5-cCQ",
+  authDomain: "telegram-clone-82d5b.firebaseapp.com",
+  databaseURL: "https://telegram-clone-82d5b.firebaseio.com",
+  projectId: "telegram-clone-82d5b",
+  storageBucket: "telegram-clone-82d5b.appspot.com",
+  messagingSenderId: "757740359018",
+  appId: "1:757740359018:web:19e235a716c285d18ba913"
+};
